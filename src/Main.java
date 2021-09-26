8e39fe84682d6660526e45ff84140555601f7e10
LROGANMAG15\antho <thesecretsmurfservice@gmail.com>
2021-09-26 16:21:10 -0500

    fleshed out Results, started defining generators in Main

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,27 @@
-public static void main(String[] args)
+public class Main
 {
+    public static void main(String[] args)
+    {
 
+    }
+
+    private int[] generateAscendingList()
+    {
+
+    }
+
+    private int[] generateDescendingList()
+    {
+
+    }
+
+    private int[] generatePartialSortedList()
+    {
+
+    }
+
+    private int[] generateRandomList()
+    {
+
+    }
 }
---
+++ src/Results.java
@@ -0,0 +1,69 @@
+public class Results
+{
+    private int n, compares, moves, time;
+    private String listType, sort;
+
+    public Results()
+    {
+        n = compares = moves = time = 0;
+        listType = sort = "";
+    }
+
+    public Results(int elements, String dataType, String sortType)
+    {
+        n = elements;
+        listType = dataType;
+        sort = sortType;
+        compares = moves = time = 0;
+    }
+
+    public Results(int elements, int comparisons, int movements, int ms, String dataType, String sortType)
+    {
+        n = elements;
+        compares = comparisons;
+        moves = movements;
+        time = ms;
+        listType = dataType;
+        sort = sortType;
+    }
+
+    public void setElements(int elements)
+    {
+        n = elements;
+    }
+
+    public void setCompares(int comparisons)
+    {
+        compares = comparisons;
+    }
+
+    public void setMovements(int movements)
+    {
+        moves = movements;
+    }
+
+    public void setTime(int ms)
+    {
+        time = ms;
+    }
+
+    public void setdataType(int dataType)
+    {
+        listType = dataType;
+    }
+
+    public void setSortType(int sortType)
+    {
+        sort = sortType;
+    }
+
+    public String toString()
+    {
+        return  "N: " + n + "\n" +
+                "DataType: " + listType + "\n" +
+                "Sort: " + sort + "\n" +
+                "Comparisons: " + compares + "\n" +
+                "Movements: " + moves + "\n" +
+                "Total Time: " + time + "\n";
+    }
+}
